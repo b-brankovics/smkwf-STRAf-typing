87b85ba68f7dbdcabc1affa921f9c27d7e0d449f
b-brankovics <6728856+b-brankovics@users.noreply.github.com>
2026-08-25 10:18:19 +0200

    Fix regex grab

--- workflow/scripts/screen.sh
+++ workflow/scripts/screen.sh
@@ -44,7 +44,8 @@ sub screen {
     # There can be imperfect repeats near the ends of the repeat region,
     # so we allow for 1 imperfect repeat at positions 2 or 3 from the end.
     my $regex0 = "^" . "(.*?)"
-      . "((?:${repeat})+(?:.{$n})?(?:${repeat})+)"    # This maybe too short
+      . "((?:${repeat}){1,}.{$n}(?:${repeat}){1,})"
+      # . "((?:${repeat})+(?:.{$n})?(?:${repeat})+)"    # This maybe too short
       . "(.*?)" . '$';
     my $regex = "^" . "(.*?)"
       . "((?:${repeat}){1,2}.{$n}(?:${repeat}){2,}.{$n}(?:${repeat}){1,2})"
